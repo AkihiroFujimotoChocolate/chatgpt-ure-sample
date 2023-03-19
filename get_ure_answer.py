170582a53b734ef33a6a668c20f38101249fffc3
Akihiro Fujimoto <akihiro.dpblc01@gmail.com>
2023-03-19 23:10:22 +0900

    On branch randomselect_knowledge Changes to be committed: 	modified:   get_ure_answer.py 	modified:   rinna_ure.py 	modified:   urechat_sample.py

--- get_ure_answer.py
+++ get_ure_answer.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-from rinna_ure import get_ure_answer
+from rinna_ure import get_ure_answers
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     parser.add_argument("--l3ReturnNum", type=int, default=1, help="The number of L3 results to return. Defaults to 1.")
     args = parser.parse_args()
 
-    results = get_ure_answer(args.query, args.knowledgeSetId, args.l2ReturnNum, args.l3ReturnNum)
+    results = get_ure_answers(args.query, args.knowledgeSetId, args.l2ReturnNum, args.l3ReturnNum)
     print(results)
--- rinna_ure.py
+++ rinna_ure.py
@@ -17,7 +17,7 @@ def upload_ure_knowledge(upload_file, is_file_overwrite=False):
     else:
         raise Exception(f"Upload failed. status code {response.status_code}")
     
-def get_ure_answer(query, knowledgeSetId, l2ReturnNum=3, l3ReturnNum=1):
+def get_ure_answers(query, knowledgeSetId, l2ReturnNum=3, l3ReturnNum=1):
     url = "https://api.rinna.co.jp/models/ure/v5.2"
     headers = {
         "Content-Type": "application/json",
@@ -36,5 +36,5 @@ def get_ure_answer(query, knowledgeSetId, l2ReturnNum=3, l3ReturnNum=1):
     json_response = response.json()
     l3_docs_list = json_response["l3DocsList"]
     l3_scores_list = json_response["l3ScoresList"]
-    results = [{"answer": l3_docs_list[0][i], "score": l3_scores_list[0][i]} for i in range(min(l3ReturnNum, len(l3_docs_list[0])))]
+    results = [{"document": l3_docs_list[0][i], "score": l3_scores_list[0][i]} for i in range(min(l3ReturnNum, len(l3_docs_list[0])))]
     return results
